0056d9dcbec3fe0517603a831b73dcf170bbac3f
soumojit banerjee <soumojitbanerjee08@gmail.com>
2025-04-29 12:01:11 +0530

    Finish root of app

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,10 +1,14 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to React-Native</Text>
+
+      <Link href="/(auth)">Login</Link>
+      <Link href="/(auth)/signup">Signup</Link>
     </View>
   );
 }
